1d35e8d5ce085a6eaead51ee3286f928ca780a8d
benbmoore <blueredorange@gmail.com>
2020-03-26 15:37:56 +0000

    Change input checks to not exception on String

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ if __name__ == "__main__":
         int(userInput)
         if int(userInput) < 0:  # If it isn't a positive number, print error message
             print("Sorry, it must be a positive number")
+        for x in range(0, int(userInput)):
+            print(fizzbuzz.fizzbuzz(x))
     except ValueError:
         print("Please Enter a whole number")
-
-    for x in range(0, int(userInput)):
-        print(fizzbuzz.fizzbuzz(x))
